0f07df79467347af1aac9e0ed26a65cb6e2eeb39
abhinavrau <abhinav.rau@gmail.com>
2017-04-04 17:05:20 -0400

    Renamed files

---
+++ src/main/java/com/github/panchitoboy/shiro/jwt/JWTService.java
@@ -0,0 +1,137 @@
+package com.github.panchitoboy.shiro.jwt;
+
+import com.github.panchitoboy.shiro.jwt.repository.TokenResponse;
+import com.github.panchitoboy.shiro.jwt.repository.UserDefault;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import org.ops4j.pax.shiro.cdi.ShiroIni;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Creates and verifies JWT tokens. For more details on JWT tokens and tutorial see
+ * <a href="https://github.com/dwyl/learn-json-web-tokens">https://github.com/dwyl/learn-json-web-tokens</a>
+ */
+@ShiroIni
+@Singleton
+@Named("jwtService")
+public class JWTService {
+
+    Logger logger = LoggerFactory.getLogger(JWTService.class);
+
+
+
+    private long expirationSeconds = 5;
+
+    private String issuer = "none";
+
+    private String secretKey = "72AC05536733581EA598CB31BA044D7D03A16B6057093DCF2B780A505607FF7"; // Needs to sufficiently long and random
+
+
+    public void setExpirationSeconds(long expirationSeconds) {
+        this.expirationSeconds = expirationSeconds;
+    }
+
+    public void setIssuer(String issuer) {
+        this.issuer = issuer;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+
+    public long getExpirationSeconds() {
+        return expirationSeconds;
+    }
+
+    public String getIssuer()
+    {
+        return issuer;
+    }
+
+    public String getSecretKey()
+    {
+        return secretKey;
+    }
+
+    public TokenResponse createToken(UserDefault user) {
+        TokenResponse response = new TokenResponse(user, createJWT(user.getPrincipal(), user.getHost(), user.getRoles()));
+        return response;
+    }
+
+    protected String createJWT(Object userId, String clientAddress, Set<String> roles) {
+
+        try {
+            JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder();
+            Date now = new Date();
+            builder.issuer(getIssuer());
+            builder.subject(userId.toString());
+            builder.issueTime(now);
+            builder.notBeforeTime(now);
+            builder.expirationTime(new Date(now.getTime() + (getExpirationSeconds() * 1000)));
+            builder.jwtID(UUID.randomUUID().toString());
+            builder.claim("client IP", clientAddress);
+            builder.claim("roles", roles.toArray());
+
+            JWTClaimsSet claimsSet = builder.build();
+
+            JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+
+            Payload payload = new Payload(claimsSet.toJSONObject());
+
+            JWSObject jwsObject = new JWSObject(header, payload);
+
+            JWSSigner signer = new MACSigner(getSecretKey());
+            jwsObject.sign(signer);
+            logger.info("Creating JWT session with expiration {} seconds", getExpirationSeconds());
+            return jwsObject.serialize();
+        } catch (JOSEException ex) {
+            logger.error("Failed creating JWT token", ex);
+            return null;
+        }
+    }
+
+    public boolean validateToken(SignedJWT signed) {
+
+        try {
+
+            MACVerifier verifier = new MACVerifier(getSecretKey());
+            return signed.verify(verifier);
+
+        } catch (JOSEException ex) {
+            logger.error("JOSEException validating signed JWT", ex);
+            return false;
+        }
+
+    }
+    public boolean isNotExpired(SignedJWT signed)
+    {
+        long time = System.currentTimeMillis();
+        boolean isNotExpired = false;
+        try {
+            isNotExpired = signed.getJWTClaimsSet().getNotBeforeTime().getTime() <= time && time < signed.getJWTClaimsSet().getExpirationTime().getTime();
+            if (false == isNotExpired)
+            {
+
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return isNotExpired;
+
+    }
+
+
+}
---
+++ src/test/java/com/github/panchitoboy/shiro/jwt/verifier/JWTServiceTest.java
@@ -0,0 +1,110 @@
+package com.github.panchitoboy.shiro.jwt.verifier;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.UUID;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class JWTServiceTest {
+
+    static byte[] sharedKey;
+
+    @BeforeClass
+    public static void testing() throws IOException {
+        SecureRandom random = new SecureRandom();
+        sharedKey = new byte[32];
+        random.nextBytes(sharedKey);
+    }
+
+    @Test
+    public void validToken() throws JOSEException, ParseException {
+        JWTClaimsSet jwtClaims = getJWTClaimsSet("issuer", "subject", new Date(), new Date(), new Date(new Date().getTime() + 100000));
+
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+
+        Payload payload = new Payload(jwtClaims.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        JWSSigner signer = new MACSigner(sharedKey);
+        jwsObject.sign(signer);
+        String token = jwsObject.serialize();
+
+        SignedJWT signed = SignedJWT.parse(token);
+        JWSVerifier verifier = new MACVerifierExtended(sharedKey, signed.getJWTClaimsSet());
+        signed.verify(verifier);
+
+        Assert.assertTrue("Must be valid", signed.verify(verifier));
+    }
+
+    @Test
+    public void invalidTokenNotBeforeTime() throws JOSEException, ParseException {
+        JWTClaimsSet jwtClaims = getJWTClaimsSet("issuer", "subject", new Date(), new Date(new Date().getTime() + 100000), new Date(new Date().getTime() + 200000));
+
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+
+        Payload payload = new Payload(jwtClaims.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        JWSSigner signer = new MACSigner(sharedKey);
+        jwsObject.sign(signer);
+        String token = jwsObject.serialize();
+
+        SignedJWT signed = SignedJWT.parse(token);
+        JWSVerifier verifier = new MACVerifierExtended(sharedKey, signed.getJWTClaimsSet());
+        signed.verify(verifier);
+
+        Assert.assertFalse("Must be invalid", signed.verify(verifier));
+    }
+
+    @Test
+    public void invalidTokenExpirationTime() throws JOSEException, ParseException {
+        JWTClaimsSet jwtClaims = getJWTClaimsSet("issuer", "subject", new Date(), new Date(), new Date());
+
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+
+        Payload payload = new Payload(jwtClaims.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        JWSSigner signer = new MACSigner(sharedKey);
+        jwsObject.sign(signer);
+        String token = jwsObject.serialize();
+
+        SignedJWT signed = SignedJWT.parse(token);
+        JWSVerifier verifier = new MACVerifierExtended(sharedKey, signed.getJWTClaimsSet());
+        signed.verify(verifier);
+
+        Assert.assertFalse("Must be invalid", signed.verify(verifier));
+    }
+
+    private JWTClaimsSet getJWTClaimsSet(String issuer, String subject, Date issueTime, Date notBeforeTime, Date expirationTime) {
+        JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder();
+        builder.issuer(issuer);
+        builder.subject(subject);
+        builder.issueTime(issueTime);
+        builder.notBeforeTime(notBeforeTime);
+        builder.expirationTime(expirationTime);
+        builder.jwtID(UUID.randomUUID().toString());
+        return builder.build();
+    }
+
+}
